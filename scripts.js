9293d46b164a2303c855d2e0d2985ece41a1502a
Deycov <53343872+adrianA-R@users.noreply.github.com>
2023-01-07 22:09:04 -0400

    asides actualizados y validados, junto con menus y algunos otros estilos css

--- scripts.js
+++ scripts.js
@@ -1,49 +1,12 @@
 //Dinamismo de la web
 
-const emailAccount = document.querySelector(".navbar-email");
-const desktopMenu = document.querySelector(".desktop-menu");
-
-const burguerButton = document.querySelector(".menu");
-const mobileMenu = document.querySelector(".mobile-menu");
-
-const shoppingIcon = document.querySelector(".navbar-shopping-cart");
-const asideProduct = document.querySelector(".product-detail");
-
-const containerProducts = document.querySelector(".cards-container");
-
-emailAccount.addEventListener('click', toggleDesktopMenu);
-burguerButton.addEventListener('click', toggleMobileMenu);
-shoppingIcon.addEventListener('click', toggleProductAside);
-
-function toggleDesktopMenu () {
-    desktopMenu.classList.toggle("inactive");
-    asideProduct.classList.add("inactive")
-}
-
-function toggleMobileMenu () {
-    mobileMenu.classList.toggle("inactive");
-    asideProduct.classList.add("inactive");
-}
-
-function toggleProductAside () {
-    const isActiveDesktop = desktopMenu.classList.contains("inactive");
-    const isActiveMobile = mobileMenu.classList.contains("inactive");
-    
-    if (!isActiveDesktop)
-        desktopMenu.classList.add("inactive");
-
-    if (!isActiveMobile)
-        mobileMenu.classList.add("inactive")
-
-    asideProduct.classList.toggle("inactive")
-}
-
 const databaseProducts = [];
 
 databaseProducts.push({name:"Bike", price:"120,00", image:"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" });
-databaseProducts.push({name:"Bike", price:"120,00", image:"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" });
-databaseProducts.push({name:"Bike", price:"120,00", image:"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" });
+databaseProducts.push({name:"Bike", price:"23,00", image:"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" });
+databaseProducts.push({name:"Bike", price:"66,40", image:"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" });
 
+const containerProducts = document.querySelector(".cards-container");
 
 function cardsProducts (database,container){
     for(product of database){
@@ -60,7 +23,7 @@ function cardsProducts (database,container){
        const price = document.createElement("P");
        price.innerText = '$' + product.price;
        const name = document.createElement("P");
-       name.innerText= '$' + product.name;
+       name.innerText= product.name;
        divInfoContainer.append(price,name);
 
        const figure = document.createElement("FIGURE");
@@ -72,11 +35,88 @@ function cardsProducts (database,container){
        divInfo.append(divInfoContainer,figure);
        divProductCard.append(image,divInfo);
        container.append(divProductCard);
+
+       const productDetail = document.querySelector(".product-detail");
+       
+
+       divProductCard.addEventListener('click', openDetails);
+       
+
+        
+        
+        
     }
 }
 
 
+
 cardsProducts(databaseProducts,containerProducts);
+
+const emailAccount = document.querySelector(".navbar-email");
+const desktopMenu = document.querySelector(".desktop-menu");
+
+const burguerButton = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+
+const shoppingIcon = document.querySelector(".navbar-shopping-cart");
+const asideProduct = document.querySelector(".shopping-cart-aside");
+
+const productCard = document.querySelector(".product-card");
+
+const productDetail = document.querySelector(".product-detail");
+const closeProductDetail = document.querySelector(".product-detail-close")
+
+
+emailAccount.addEventListener('click', toggleDesktopMenu);
+burguerButton.addEventListener('click', toggleMobileMenu);
+shoppingIcon.addEventListener('click', toggleProductAside);
+closeProductDetail.addEventListener('click', closeDetails);
+
+function closeDetails(){
+    productDetail.classList.add("inactive");
+}
+
+function toggleDesktopMenu () {
+    desktopMenu.classList.toggle("inactive");
+    asideProduct.classList.add("inactive");  
+}
+
+function toggleMobileMenu () {
+    mobileMenu.classList.toggle("inactive");
+    asideProduct.classList.add("inactive");
+    productDetail.classList.add("inactive");
+}
+
+function toggleProductAside () {
+    const isActiveDesktop = desktopMenu.classList.contains("inactive");
+    const isActiveMobile = mobileMenu.classList.contains("inactive");
+    
+    if (!isActiveDesktop)
+        desktopMenu.classList.add("inactive");
+
+    if (!isActiveMobile)
+        mobileMenu.classList.add("inactive");
+
+    asideProduct.classList.toggle("inactive");
+    productDetail.classList.add("inactive");
+}
+
+function openDetails () {
+    // const productPrice = document.createElement('P');
+    // productPrice.innerText = '$' + product.price;
+
+    // const productName = document.createElement('P');
+    // productName.innerText = product.name;
+
+    // const productInfo = document.querySelector(".product-info");
+
+    // productInfo.append(productPrice, productName);
+
+    productDetail.classList.remove("inactive");
+    asideProduct.classList.add("inactive");
+}
+
+
 /*
 <div class="product-card">
     <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
